acf5ceb808a0df5da030c29f062e5b687eef700e
Henrique Paulino <84615435+hapaulino@users.noreply.github.com>
2021-06-04 20:11:59 +0100

    Update mid.js

--- smokes/mirage/mid.js
+++ smokes/mirage/mid.js
@@ -1,16 +1,7 @@
-<<<<<<< Updated upstream
 module.exports = {
     name: 'mirage-mid',
 	description: 'Top Mid smoke from T Spawn.',
 	aliases:  ['mrg-mid'],
 	execute(message, args) {
 			if (!args.lenght){ 
-=======
-module.exports = {
-    name: 'mirage-mid',
-	description: 'Top Mid smoke from T Spawn.',
-	aliases:  ['mrg-mid'],
-	execute(message, args) {
-			if (!args.lenght){ 
->>>>>>> Stashed changes
 			message.channel.send('https://gfycat.com/nearfriendlycollardlizard')}}};

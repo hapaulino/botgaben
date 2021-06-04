331c15f0a276f6b8bcb5f99b075bad601fa12a53
Henrique Paulino <84615435+hapaulino@users.noreply.github.com>
2021-06-04 20:14:41 +0100

    Update window-spawn.js

--- smokes/mirage/window-spawn.js
+++ smokes/mirage/window-spawn.js
@@ -1,16 +1,7 @@
-<<<<<<< Updated upstream
 module.exports = {
     name: 'mirage-window-spawn',
 	description: 'Window smoke from T Spawn.',
 	aliases:  ['mrg-window-spawn'],
 	execute(message, args) {
 			if (!args.lenght){ 
-=======
-module.exports = {
-    name: 'mirage-window-spawn',
-	description: 'Window smoke from T Spawn.',
-	aliases:  ['mrg-window-spawn'],
-	execute(message, args) {
-			if (!args.lenght){ 
->>>>>>> Stashed changes
 			message.channel.send('https://gfycat.com/whitefittingliger')}}};

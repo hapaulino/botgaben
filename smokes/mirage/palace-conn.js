3ad9a38437b6e6e66976cf73741285b553ce9248
Henrique Paulino <84615435+hapaulino@users.noreply.github.com>
2021-06-04 20:12:12 +0100

    Update palace-conn.js

--- smokes/mirage/palace-conn.js
+++ smokes/mirage/palace-conn.js
@@ -1,16 +1,7 @@
-<<<<<<< Updated upstream
 module.exports = {
     name: 'mirage-palace-conn',
 	description: 'Palace smoke from Top Connector.',
 	aliases:  ['mrg-palace-conn'],
 	execute(message, args) {
 			if (!args.lenght){ 
-=======
-module.exports = {
-    name: 'mirage-palace-conn',
-	description: 'Palace smoke from Top Connector.',
-	aliases:  ['mrg-palace-conn'],
-	execute(message, args) {
-			if (!args.lenght){ 
->>>>>>> Stashed changes
 			message.channel.send('https://gfycat.com/farflunggregariouscatbird')}}};

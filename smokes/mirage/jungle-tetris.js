b0fbe01ad68003c018d3cac05590ff89ce6659ca
Henrique Paulino <84615435+hapaulino@users.noreply.github.com>
2021-06-04 20:10:51 +0100

    Add files via upload

---
+++ smokes/mirage/jungle-tetris.js
@@ -0,0 +1,16 @@
+<<<<<<< Updated upstream
+module.exports = {
+    name: 'mirage-jungle-tetris',
+	description: 'Jungle smoke from Tetris.',
+	aliases:  ['mrg-jungle-tetris'],
+	execute(message, args) {
+			if (!args.lenght){ 
+=======
+module.exports = {
+    name: 'mirage-jungle-tetris',
+	description: 'Jungle smoke from Tetris.',
+	aliases:  ['mrg-jungle-tetris'],
+	execute(message, args) {
+			if (!args.lenght){ 
+>>>>>>> Stashed changes
+			message.channel.send('https://gfycat.com/diligentvengefuliberianmole')}}};

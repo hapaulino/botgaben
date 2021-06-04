a0b47b2968a8165dda7eafb300a9a74fe9de829f
Henrique Paulino <84615435+hapaulino@users.noreply.github.com>
2021-06-04 20:12:31 +0100

    Update palace-kitchen.js

--- smokes/mirage/palace-kitchen.js
+++ smokes/mirage/palace-kitchen.js
@@ -1,16 +1,7 @@
-<<<<<<< Updated upstream
 module.exports = {
     name: 'mirage-palace-kitchen',
 	description: 'Palace support smoke from Outside Kitchen.',
 	aliases:  ['mrg-palace-kitchen'],
 	execute(message, args) {
 			if (!args.lenght){ 
-=======
-module.exports = {
-    name: 'mirage-palace-kitchen',
-	description: 'Palace support smoke from Outside Kitchen.',
-	aliases:  ['mrg-palace-kitchen'],
-	execute(message, args) {
-			if (!args.lenght){ 
->>>>>>> Stashed changes
 			message.channel.send('https://gfycat.com/indolentinsidiousdrongo')}}};

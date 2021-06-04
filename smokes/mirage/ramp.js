434b532652b2c66863bdfeb1393e969e98e85bdf
Henrique Paulino <84615435+hapaulino@users.noreply.github.com>
2021-06-04 20:13:15 +0100

    Update ramp.js

--- smokes/mirage/ramp.js
+++ smokes/mirage/ramp.js
@@ -1,16 +1,7 @@
-<<<<<<< Updated upstream
 module.exports = {
     name: 'mirage-ramp',
 	description: 'Ramp smoke from CT.',
 	aliases:  ['mrg-ramp'],
 	execute(message, args) {
 			if (!args.lenght){ 
-=======
-module.exports = {
-    name: 'mirage-ramp',
-	description: 'Ramp smoke from CT.',
-	aliases:  ['mrg-ramp'],
-	execute(message, args) {
-			if (!args.lenght){ 
->>>>>>> Stashed changes
 			message.channel.send('https://gfycat.com/lavishthesebasilisk')}}};
